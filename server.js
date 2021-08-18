9270ca78a813ad1be50f93840083693f3d3634de
eunzgi <dmswl6842@naver.com>
2021-08-18 19:56:18 +0900

    update
    error

--- server.js
+++ server.js
@@ -1,30 +1,46 @@
-//router
+//모듈 임포트
 var express = require('express');
 var app = express();
 var fs = require('fs');
 var session = require('express-session');
 var db = require('./db');
+var morgan = require('morgan');           //로그 모듈
 var bodyParser = require('body-parser');
 var compression = require('compression');
 var helmet = require('helmet');
  
+//라우트로 분리
 var indexRouter = require('./routes/index');
 var topicRouter = require('./routes/topic');
-//const template = require('./template');
+//var userRouter = require('./routes/user.js');
  
-// app.set('views', __dirname + '/views'); // 서버가 읽을 수 있도록 HTML 의 위치를 정의해줍니다.
-// app.set('view engine', 'ejs');          // 서버가 HTML 렌더링을 할 때, EJS엔진을 사용하도록 설정합니다. 출처: https://til0804.tistory.com/17 []
+app.set('views', __dirname + '/views'); // 서버가 읽을 수 있도록 HTML 의 위치를 정의
+app.set('view engine', 'ejs');          // 서버가 HTML 렌더링을 할 때, EJS엔진을 사용하도록 설정
 // app.engine('html', require('ejs').renderFile);
 
-app.use(express.static('public'));
+// 미들웨어 설정
+app.use(express.static('public'));          
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
-app.get('*', function(request, response, next) {
+app.use(morgan('short'))
+
+var productRouter = require('./routes/topic.js')
+//app.use(userRouter)
+app.use(productRouter)
+ 
+/*
+app.get('*', function(req, res, next) {
    fs.readdir('./data', function(error, filelist ){
-     request.list = filelist;
+     req.list = filelist;
      next();
     });
- });
+    res.render('list.ejs', {'list':req.list}, function(err,html) {
+      if (err) {
+        console.log(err)
+      }
+      res.end(html)
+    });
+  });
  
 app.use('/', indexRouter);
 app.use('/topic', topicRouter);
@@ -37,7 +53,7 @@ app.use(function (err, req, res, next) {
   console.error(err.stack)
   res.status(500).send('Something broke!')
 });
- 
+ */
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!')
 });
